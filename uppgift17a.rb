d86be5bb48f74f7a864b0859cdf71db102d0fd6b
itgsod-jovan-cvetic <jovan.cvetic@elev.it-gymnasiet.se>
2015-03-24 14:30:29 +0100

    hej

--- uppgift17a.rb
+++ uppgift17a.rb
@@ -9,3 +9,28 @@ require 'colorize'
 #   dice1=rand(1..6)
 #   dice2=rand(1..6)
 #
+#   if (dice1-dice2).abs == 3
+#     diff3 += 1
+#
+#   end
+#
+#     end
+#
+#
+# puts diff3/randoms.to_f
+
+  diff3 = 0
+
+  (1..6).each do |dice1|
+    (1..6).abs == 4
+      if (dice1-dice2).abs == 4
+        diff3 += 1
+        print "#{dice1},#{dice2} ".red
+      else
+        print "#{dice1},#{dice2} ".blue
+      end
+  end
+  puts
+end
+
+puts diff3/36.0
